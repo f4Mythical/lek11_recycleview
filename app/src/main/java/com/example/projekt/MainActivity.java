3e49a412a35698ef54d23848c9f97f100ccdad35
F4Mythical <grynka.adam@gmail.com>
2026-02-03 08:52:19 +0100

    dodane wartosci w mainactivty

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -7,9 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+private ArrayList<Produkt> produkty;
+private RecyclerView recyclerView;
+private ZakupyAdapter zakupyAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+        produkty.add(new Produkt("Ok",5.0));
+        produkty.add(new Produkt("to",5));
+        produkty.add(new Produkt("nie",5));
+        produkty.add(new Produkt("jest",5));
+        produkty.add(new Produkt("jakby",5));
+        produkty.add(new Produkt("okay",5));
+        produkty.add(new Produkt("czaisz",5));
+        produkty.add(new Produkt("to",5));
+        zakupyAdapter = new ZakupyAdapter(MainActivity.this,produkty);
+        recyclerView.setAdapter(zakupyAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
