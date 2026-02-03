307532464c1fe97e42049ca32671cc72bb952c29
F4Mythical <grynka.adam@gmail.com>
2026-02-03 08:20:46 +0100

    wygenerowanie konstruktora i dodanie reszty rzeczy do zakupyadapter

--- app/src/main/java/com/example/projekt/ZakupyAdapter.java
+++ app/src/main/java/com/example/projekt/ZakupyAdapter.java
@@ -1,4 +1,44 @@
 package com.example.projekt;
 
-public class ZakupyAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ZakupyAdapter extends RecyclerView.Adapter<ZakupyAdapter.ProduktViewHolder>     {
+    private  ArrayList<Produkt> produkty;
+    private LayoutInflater inflater;
+
+    public ZakupyAdapter(Context context, ArrayList<Produkt> produkty) {
+        inflater = LayoutInflater.from(context);
+        this.produkty = produkty;
+    }
+
+    @NonNull
+    @Override
+    public ProduktViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProduktViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ProduktViewHolder extends RecyclerView.ViewHolder {
+
+        public ProduktViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
